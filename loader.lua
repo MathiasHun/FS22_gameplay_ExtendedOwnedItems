4f99cbcfb4d70d2164a6cafeade71ba64a2997ba
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2024-08-09 10:20:25 +0200

    1.0.0.0

--- loader.lua
+++ loader.lua
@@ -1,36 +1,12 @@
 
-source(Utils.getFilename("extendedOwnedItems.lua", g_currentModDirectory))
-source(Utils.getFilename("initialize.lua", g_currentModDirectory))
+local specName = g_currentModName..".extendedOwnedItems"
 
-extendedOwnedItems:Initialize()
-
-function installSpec()
-	if g_specializationManager:getSpecializationByName("extendedOwnedItems") == nil then
-		if extendedOwnedItems == nil then 
-			print("ERROR: unable to find source file 'extendedOwnedItems.lua'")
-		else 
-			for typeName, typeDef in pairs(g_vehicleTypeManager.types) do
-				if typeDef ~= nil and (typeName ~= "locomotive") then 
-					local isWashable = false
-					local isAnimated = false
-					for name, spec in pairs(typeDef.specializationsByName) do
-						if name == "washable" then 
-							isWashable = true
-						elseif name == "animatedVehicle" then 
-							isAnimated = true
-						end
-					end
-					if isWashable and isAnimated then
-						 if typeDef.specializationsByName["extendedOwnedItems"] == nil then
-							table.insert(typeDef.specializations, extendedOwnedItems)
-							table.insert(typeDef.specializationNames, "extendedOwnedItems")
-							typeDef.specializationsByName["extendedOwnedItems"] = extendedOwnedItems
-						end
-					end
-				end
-			end
-		end
-	end
+if g_specializationManager:getSpecializationByName("extendedOwnedItems") == nil then
+	g_specializationManager:addSpecialization("extendedOwnedItems", "extendedOwnedItems", g_currentModDirectory.."extendedOwnedItems.lua", nil)
 end
 
-TypeManager.validateTypes = Utils.appendedFunction(TypeManager.validateTypes, installSpec)
+for typeName, typeEntry in pairs(g_vehicleTypeManager.types) do
+	if SpecializationUtil.hasSpecialization(Washable, typeEntry.specializations) and SpecializationUtil.hasSpecialization(AnimatedVehicle, typeEntry.specializations) then
+		g_vehicleTypeManager:addSpecialization(typeName, specName)
+	end
+end
