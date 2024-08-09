7cb39f72f3d2133f04bf11368a52cd68070d40eb
MathiasHU <30857803+MathiasHun@users.noreply.github.com>
2024-08-09 10:19:03 +0200

    1.0.0.0

--- initialize.lua
+++
@@ -1,9 +0,0 @@
-
-function extendedOwnedItems:Initialize()
-	extendedOwnedItems.modDirectory  = g_currentModDirectory
-	local modDesc = loadXMLFile("modDesc", extendedOwnedItems.modDirectory .. "modDesc.xml")
-	extendedOwnedItems.version = getXMLString(modDesc, "modDesc.version")
-	extendedOwnedItems.title = getXMLString(modDesc, "modDesc.title.en")
-	extendedOwnedItems.author = getXMLString(modDesc, "modDesc.author")
-	delete(modDesc)
-end
